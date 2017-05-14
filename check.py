1559b4caa57e53bc59cd6e2d54d46475c1238af9
袁至誠 <r1218r1218@gmail.com>
2017-05-14 18:16:22 +0800

    remove nonascii

--- check.py
+++ check.py
@@ -2,11 +2,6 @@ from xpinyin import Pinyin
 from nltk.tokenize.util import is_cjk
 from random import choice
 
-'''
->>> get_pinyin("女驢呂律拘局舉句需徐許續瘀魚雨玉")
-    'nv-lv-lv-lv-ju-ju-ju-ju-xu-xu-xu-xu-yu-yu-yu-yu'
-'''
-
 p = Pinyin()
 
 golden_sound = ['a','an','ou','yi','wu','wa','ba','ban','bei','bi','bu','pa','pan','pou','pei','pi','pu','ma','man','mou','mei','mi','mu','fa','fan','fou','fei','fu','da','dan','dou','dei','di','du','ta','tan','you','ti','tu','na','nan','nou','nei','ni','nu','la','lan','lou','lei','li','lu','za','zan','zou','zei','zi','sa','san','sou','si','su','cha','chan','chou','chi','chu','sha','shan','shou','shei','shu','shua','ran','rou','ru','ji','ga','gan','gou','gei','gu','gua','ka','kan','kou','kei','ku','kua','ha','han','hou','hei','hu','hua']
@@ -14,9 +9,7 @@ good_sound = ['a','ai','ao','an','o','ou','e','en','yi','you','yan','yang','yin'
 warning_sound = ['ang','eng','er','weng','yun','bang','beng','bie','pang','pie','mang','mie','dang','die','diu','dui','duan','teng','tie','tuo','tui','tuan','nang','nene','nie','niang','nin','nuo','nuan','leng','liang','luo','luan','zai','zhai','zhao','zhan','zhang','zhong','zhou','zhe','zhei','zhen','zheng','zhi','zhu','zhua','zhuo','zhui','zhuai','zhuan','zhun','zhuang','cheng','chua','chui','chuai','chuan','chun','chuang','sheng','shui','shuai','shuan','shuang','reng','ri','rua','rui','ruan','geng','guo','gui','guan','keng','kuo','kui','kuan','heng','huo','hui','huai','huan']
 bad_sound = ['yue','yuan','nu','nue','lu','lue','ca','cai','cao','can','cang','cong','cou','ce','cen','ceng','ci','cu','cuo','cui','cuan','cun','shi','jie','jiong','ju','jue','juan','qi','qia','qiao','qie','qiu','qian','qiang','qin','qing','qiong','qu','que','quan','qun','xi','xia','xiao','xie','xiu','xian','xiang','xin','xing','xiong','xu','xue','xuan','xun']
 
-#bad_chinese = set(map(p.get_pinyin, input("bad chinese?")))
-#bad_chinese = set(map(p.get_pinyin, "女驢呂律拘局舉句需徐許續瘀魚雨玉姜辞职去腿四十知道汉语日轮喝水"))
-'''
+
 subject = input("enter:")
 for s in subject:
     if is_cjk(s):
@@ -27,7 +20,7 @@ for s in subject:
             print(s, 'is okay')
         elif t in bad_sound:
             print(s, 'is bad')
-'''
+
 l = int(input("len:"))
 n = ''
 for _ in range(l):
